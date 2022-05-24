387dd902c14c75b0de70e950a2978e159c5899f8
DZhang3210 <53838139+DZhang3210@users.noreply.github.com>
2022-05-24 12:54:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,23 @@ from MinHeap import *
 #14 Represents diagonal movement
 dist = [10, 14]
 start, end = (0,0), (5,4)
+#representing all movement directions
+dir = [[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1],[0,1]]
+
+#0 Stands for open space
+#1 Stands for Flag
+#2 Stands for obstacle
+board = [
+    [1,0,0,0,0,0]
+    [0,2,0,0,0,0]
+    [2,2,2,2,0,0]
+    [0,0,0,0,0,0]
+    [0,0,0,0,0,1]
+]
 
 class Node:
-    def __init__ (self, prev, val, coords):
+    def __init__ (self, prev, coords):
         self.prev = prev
-        self.val = val
         self.coords = coords
 
         #Main way of prioritizing and calculating next node in heap
@@ -18,38 +30,39 @@ class Node:
         self.F = self.G+self.H
 
     #Reassigning variables in case of new calculation
-    def reev(self, prev, val, G, F):
+    def reev(self, prev, val, G):
         self.prev = prev
         self.val = val
         #If we are reevaluating Node then that means that prev G path must've
         #been updated
-        self.G = G
+        self.G = G + prev.G
         self.F = self.G + self.H
 
     #Evaluating all import values from start/end
-    def getValue(self, point, parent = None):
+    def getValue(self, p, point, parent = None):
         total = 0
-        x, y = self.coords[0], self.coords[1]
+        x, y = p[0], p[1]
         while(point[0] != x or point[1] != y):
             if(point[0] != x and point[1] != y):
                 total += dist[1]
             else:
-                total+= dist[0]
+                total += dist[0]
         return total
 
+    #Returns all surrounding Nodes to be returned to minHeap
+    def getSurroundingNodes(self):
+        currentX, currentY = self.coords[0], self.coords[1]
+        nodes = []
+        for i in dir:
+            currentX += i[0]
+            currentY += i[1]
+            if(currentX > 0 and currentX < len(board[0])
+                and currentY > 0 and currentY < len(board)
+                and board[currentX][currentY] != 1):
+                    nodes.append(Node(self, (currentX, currentY)))
 
-#representing all movement directions
-dir = [[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1],[0,1]]
-#must calculate distance away from starting and away from ending
+            currentX, currentY = self.coords[0], self.coords[1]
 
-#0 Stands for open space
-#1 Stands for Flag
-#2 Stands for obstacle
-list = [
-    [1,0,0,0,0,0]
-    [0,2,0,0,0,0]
-    [2,2,2,2,0,0]
-    [0,0,0,0,0,0]
-    [0,0,0,0,0,1]
-]
+
+
 
