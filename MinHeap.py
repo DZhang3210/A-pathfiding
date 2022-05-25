19392d20df6cd380e503d565aa33ea00099df4ce
DZhang3210 <53838139+DZhang3210@users.noreply.github.com>
2022-05-25 17:47:27 -0400

    Drawing the board

--- MinHeap.py
+++ MinHeap.py
@@ -1,4 +1,18 @@
 from main import *
+import pygame
+from pygame.locals import *
+from MinHeap import *
+
+pygame.init()
+display_width = 603
+display_height = 700
+dif = display_width//100
+game_display = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption("Pathfinding")
+game_display.fill((255,255,255))
+font = pygame.font.Font('freesansbold.ttf', 32)
+font1 = pygame.font.Font('freesansbold.ttf', 16)
+
 
 
 def heapifyDel(arr, n, i):
@@ -78,46 +92,68 @@ def printPath(vector):
 
 def printHeap(lst):
     for i in range(n):
-        print("(", c.coords[1],",",c.coords[0],")", end = " ")
+        print("(", i.coords[1],",",i.coords[0],")", end = " ")
         print(" : ", lst[i].F,"other", lst[i].H)
 
 
 # nodes = [Node(None, start)]
 
 #C represent the current Node
+def pathFinding():
+    #Always choose the minimum object
+    while len(nodes) > 0:
+        c = deleteRoot(nodes)
+        while (len(nodes) > 0 and checked[c.coords].checked == True):
+            c = deleteRoot(nodes)
+        checked[c.coords].checked = True
 
+        if (c.coords == end):
+            print("Done DUN DUN DUUUN")
+            reverseChain(c)
+            break
 
+        #print("Checking (", c.coords[1],",",c.coords[0],")")
+        for i in c.getSurroundingNodes():
+            if not(i.coords in checked.keys()):
+                #print("Inserting element ", i.coords, "through New")
+                checked[i.coords] = i
+                insertNode(nodes, checked[i.coords])
+            elif(checked[i.coords].checked == False):
+                #Now there are two of the same node in the object
+                #print("Inserting element ", i.coords, "through Extra")
+                if checked[i.coords].reev(c):
+                    insertNode(nodes, checked[i.coords])
 
-#THIS REPRESENTS THE MAIN FUNCTION WHICH USES A* PATHFINDING
+        #Check over any duplicate Nodes
+    if(n == 0):
+        print("Can't Find Ending")
 
 nodes = []
 checked = {start:Node(None, start)}
 nodes = [Node(None, start)]
-n=1
-#Always choose the minimum object
-while len(nodes) > 0:
-    c = deleteRoot(nodes)
-    while (len(nodes) > 0 and checked[c.coords].checked == True):
-        c = deleteRoot(nodes)
-    checked[c.coords].checked = True
+n = 1
+#pathFinding()
+tile_size = 20
+hBuffer, vBuffer, spacing = display_width//(8*tile_size),display_height//(5*tile_size),1
+xBegin, xEnd = hBuffer, (display_width//tile_size) - hBuffer
+yBegin, yEnd = vBuffer, (display_height//tile_size)
+for x in range(xBegin, xEnd):
+    for y in range(yBegin, yEnd):
+        pygame.draw.rect(game_display, (0, 155,155) , [x*tile_size, y*tile_size, tile_size-spacing, tile_size-spacing], 0)
+boldness = 3;
+pygame.draw.line(game_display, (0, 0, 0), (xBegin*tile_size,yBegin*tile_size), (xEnd*tile_size, yBegin*tile_size), boldness)
+pygame.draw.line(game_display, (0, 0, 0), (xBegin*tile_size, yBegin*tile_size), (xBegin*tile_size, yEnd*tile_size), boldness)
+pygame.draw.line(game_display, (0, 0, 0), (xEnd*tile_size, yBegin*tile_size), (xEnd*tile_size, yEnd*tile_size), boldness)
+#pygame.draw.line(game_display, (0, 0, 0), (2*display_width//3, 0), (2*display_width//3, display_width), boldness)
 
-    if (c.coords == end):
-        print("Done DUN DUN DUUUN")
-        reverseChain(c)
-        break
-
-    #print("Checking (", c.coords[1],",",c.coords[0],")")
-    for i in c.getSurroundingNodes():
-        if not(i.coords in checked.keys()):
-            #print("Inserting element ", i.coords, "through New")
-            checked[i.coords] = i
-            insertNode(nodes, checked[i.coords])
-        elif(checked[i.coords].checked == False):
-            #Now there are two of the same node in the object
-            #print("Inserting element ", i.coords, "through Extra")
-            if checked[i.coords].reev(c):
-                insertNode(nodes, checked[i.coords])
-
-    #Check over any duplicate Nodes
-if(n == 0):
-    print("Can't Find Ending")
+while True:
+    pygame.display.update()
+    for event in pygame.event.get():
+        # Check if you need to quit program
+        if (event.type == QUIT or (
+                event.type == KEYDOWN and (
+                event.key == K_ESCAPE or
+                event.key == K_q
+        ))):
+            pygame.quit()
+            quit()
